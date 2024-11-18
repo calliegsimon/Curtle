15c074c8c7aefc78fc9c7800e53ec1419ea3913a
Callie Simon <callies2003@gmail.com>
2024-11-17 20:10:53 -0600

    addition of curtle image processing functions for testing
    note: also removed main and image_processing.py for redundancy

---
+++ curtle.py
@@ -0,0 +1,90 @@
+#imports needed
+import os
+
+#importing for image wrangling
+import numpy as np
+#import pandas as pd
+#importing OpenCV
+import cv2
+
+#from PIL import Image, ImageFilter
+import turtle as t
+
+#for future reference in reagrds to making gifs of the turtle drawing
+#https://zulko.github.io/moviepy/getting_started/videoclips.html
+
+#note to self, we may need to do some preprocessing for large
+#images so that wed ont overwhelm the curtle
+def image_processing(inputImg):
+    """
+    This function is our main image processing function
+    1. will convert an image to grayscale
+    2. will apply a gaussian blur 
+    3. apply adaptive thresholding
+    4. find the contours 
+    5. return the coordinates and hierarchy of the contours
+    """
+    #converting our input image to grayscale
+    grayImg = cv2.cvtColor(inputImg, cv2.COLOR_BGR2GRAY)
+
+    #apply gaussian blur
+    blurImg = cv2.GaussianBlur(grayImg, (7,7), 0)
+
+    # using adaptive thresholding
+    threshMeanImg = cv2.adaptiveThreshold(blurImg, 255, 
+        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+
+    return threshMeanImg
+
+def contours(inputImg):
+    """ 
+    - The contours function will take in an already thresholded image
+        and find the coordinates of the contours of that image.
+    - contours will likely use the retr list or retr comp, whichever ends up working
+        best for our turtle drawing logic
+    - The contours function will then return the list of coordinates and the shape of the image
+    """
+    #init the coord list
+    coords = []
+    # find the shape of our input image
+    h, w = inputImg.shape[:2]
+    # 1. find the contours of our image
+    #default is using retr_list
+    contours, hierarchy = cv2.findContours(inputImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    #Uncomment this line if we want to switch the mode
+    #contours, hierarchy = cv2.findContours(inputImg, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+    # 2. extract the coordinates of the white regions of our image
+    white_coords = []
+    for contour in contours:
+        coordinates = contour.squeeze().astype(float).tolist()
+        white_coords.append(coordinates)
+
+    # 3. writing the coordinates of each white region to our coords list
+    for i, region in enumerate(white_coords):
+        if type(region[0]) is list:
+            if len(region) > 2:
+             # Calculate the area of the contour
+                area = cv2.contourArea(np.around(np.array([[pnt] for pnt in region])).astype(np.int32))
+                if area > 100:
+                    # Convert coordinates to the required format
+                    crdnts = [{'x': i[0], 'y': i[1]} for i in region]
+                    coords.append(crdnts)
+
+    return coords, h, w
+    
+
+
+
+#TODO: curtle logic :D
+def curtle_drawing(coordinates, h, w):
+    """curtle will take in a coordinates list 
+    coordinates list will be gotten from a contours function
+    
+    Sample of expected coordinate format:
+    [[{'x': 95.0, 'y': 693.0}, {'x': 94.0, 'y': 694.0}, {'x': 93.0, 'y': 694.0},
+    {'x': 92.0, 'y': 694.0}, {'x': 91.0, 'y': 694.0}, {'x': 90.0, 'y': 694.0}, {'x': 89.0, 'y': 695.0}
+    
+    """
+
+    #the video portion of the turtle drawing will likely als
