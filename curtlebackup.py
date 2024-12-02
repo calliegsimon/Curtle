a814cac77aa29965b5a8cd38cf7df0f22ec0ae25
Callie Simon <callies2003@gmail.com>
2024-12-02 11:16:57 -0600

    addition of curtlebackup logic for saving images

--- curtlebackup.py
+++ curtlebackup.py
@@ -6,12 +6,23 @@ import numpy as np
 #import pandas as pd
 #importing OpenCV
 import cv2
-
-#from PIL import Image, ImageFilter
+#importing PIL for image saving portion and gif saving
+from PIL import Image
+#turtle mod
 import turtle as t
 import random
-#for future reference in reagrds to making gifs of the turtle drawing
-#https://zulko.github.io/moviepy/getting_started/videoclips.html
+
+"""
+Error notes: 
+    If you recieve an error "OSError: Unable to locate Ghostscript on paths" you wioll need to install ghostscript
+    1. Install Ghostscript for your OS
+    2. Add Ghostscript to the system path (if it is not done automatically)
+        - my path i will be adding to path looks like this "C:\Program Files (x86)\gs\gs10.04.0\bin"
+        - you should only need the path to the bin folder
+    3. verify installation
+        - run gswin64c -version in cmd prompt
+    4. restart and rerun
+    """
 
 #note to self, we may need to do some preprocessing for large
 #images so that wed ont overwhelm the curtle
@@ -79,21 +90,25 @@ def extract_contours(inputImg):
 
 
 #TODO: curtle logic :D
-def curtle_drawing(coordinates, h, w):
-    """curtle will take in a coordinates list 
-    coordinates list will be gotten from a contours function
-    
-    Sample of expected coordinate format:
-    [[{'x': 95.0, 'y': 693.0}, {'x': 94.0, 'y': 694.0}, {'x': 93.0, 'y': 694.0},
-    {'x': 92.0, 'y': 694.0}, {'x': 91.0, 'y': 694.0}, {'x': 90.0, 'y': 694.0}, {'x': 89.0, 'y': 695.0}
-    
+def curtle_drawing(coordinates, h, w, finalImgName):
     """
+    curtle will take in a coordinates list 
+    coordinates list will be gotten from the contours function
+    
+    Image saving and gif saving portions will also be handled here
+    Links:
+    https://pillow.readthedocs.io/en/stable/reference/Image.html
+    https://www.geeksforgeeks.org/python-pil-image-alpha_composite-method/
+    https://www.geeksforgeeks.org/create-and-save-animated-gif-with-python-pillow/
+    https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
 
-    #the video portion of the turtle drawing will likely also be in here i assume atm
-    #same with the file? if not itll be in main
+    """
 
     colors = ["white","red", "yellow", "orange"]
     
+    #init list for frames for saving the gif
+    gif_frames = []
+
     # make canvas
     canvas = t.Screen()
     canvas.mode("world")
@@ -122,11 +137,42 @@ def curtle_drawing(coordinates, h, w):
             
             spot[1] = h - spot[1]
             pencil.goto(spot)
+
+            # capture the current frame/drawing result for the GIF
+            # here we use ps since turtle doesnt allow for jpg/png saving natively
+            # saving the color as well
+            canvas.getcanvas().postscript(file="frame.ps", colormode='color')
+            # converting the .ps file to RGBA
+            frame = Image.open("frame.ps").convert("RGBA")
+            #create a new black background image so we can save results with the black background
+            background = Image.new("RGBA", frame.size, "black")
+            #using alpha composite 
+            gifFrame = Image.alpha_composite(background, frame)
+            # append the frame 
+            gif_frames.append(gifFrame)
+
         #print ("endline")
         canvas.update()
         pencil.penup()
+    
+    #final canvas update
     canvas.tracer(1)
     canvas.update()
+
+    # saving the final drawing result as an image before we continue 
+    #similiar process to gif, only we will be saving the end version
+    canvas.getcanvas().postscript(file="results.ps", colormode='color')
+    results = Image.open("results.ps").convert("RGBA")
+    background = Image.new("RGBA", results.size, "black")
+    finalImg = Image.alpha_composite(background, results)
+    finalImg.save(f"processed/{finalImgName}_drawing.png")
+
+    #saving the stop-motion animated gif
+    #we may need to adjust duration
+    gif_frames[0].save(f"processed/{finalImgName}.gif", save_all=True,
+                        append_images=gif_frames[1:], duration=10, loop=0)
+    
+    #input to continue to the next drawing process
     input("Press enter to continue: ")
 
 
@@ -154,12 +200,11 @@ if __name__ == '__main__':
                 coords, cH, cW = extract_contours(processedImg)
                 print(f"Image #{count}'s contours extracted")
 
+                #generating our imagefinalname field to pass to curtle_drawing
+                # this is just using our same count format
+                saveImgName = f"results_{count}"
                 #drawing function for image
-                curtle_drawing(coords, cH, cW)
-
-                #save the turtle img
-
-                #saving the gif of turtle drawing
+                curtle_drawing(coords, cH, cW, saveImgName)
 
                 #update count for sequential file formats(assuming we use this)
                 count += 1
