3d4e160f4e57c879ba8e16a6df57ec4fac16a9a6
maskynon <65579995+maskynon@users.noreply.github.com>
2024-12-02 20:15:11 -0600

    names

--- curtle.py
+++ curtle.py
@@ -1,17 +1,16 @@
 #imports needed
 import os
-
+import io
 #importing for image wrangling
 import numpy as np
 #import pandas as pd
 #importing OpenCV
 import cv2
-
-#from PIL import Image, ImageFilter
+#importing PIL for image saving portion and gif saving
+from PIL import Image
+#turtle mod
 import turtle as t
 import random
-#for future reference in reagrds to making gifs of the turtle drawing
-#https://zulko.github.io/moviepy/getting_started/videoclips.html
 
 #note to self, we may need to do some preprocessing for large
 #images so that wed ont overwhelm the curtle
@@ -79,26 +78,36 @@ def extract_contours(inputImg):
 
 
 #TODO: curtle logic :D
-def curtle_drawing(coordinates, h, w):
-    """curtle will take in a coordinates list 
-    coordinates list will be gotten from a contours function
-    
-    Sample of expected coordinate format:
-    [[{'x': 95.0, 'y': 693.0}, {'x': 94.0, 'y': 694.0}, {'x': 93.0, 'y': 694.0},
-    {'x': 92.0, 'y': 694.0}, {'x': 91.0, 'y': 694.0}, {'x': 90.0, 'y': 694.0}, {'x': 89.0, 'y': 695.0}
-    
+def curtle_drawing(coordinates, h, w, finalImgName):
     """
-
-    #the video portion of the turtle drawing will likely also be in here i assume atm
-    #same with the file? if not itll be in main
-
-    colors = ["white","red", "yellow", "orange"]
+    curtle will take in a coordinates list 
+    coordinates list will be gotten from the contours function
     
+    Image saving and gif saving portions will also be handled here
+    Links:
+    https://pillow.readthedocs.io/en/stable/reference/Image.html
+    https://www.geeksforgeeks.org/python-pil-image-alpha_composite-method/
+    https://www.geeksforgeeks.org/create-and-save-animated-gif-with-python-pillow/
+    https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
+    https://docs.python.org/3/library/io.html
+
+    As I was working through this, kept running into issues with images not saving right.
+    To avoid this issues, going to change colors around and set the turtle background to white instead.
+
+    """
+    colors = ["red", "orange", "#B0578D","#3E001F","#7A316F","#624E88","#6A1E55","#C65BCF",
+            "#91D18B", "#243D25", "#5F7464"]
+    #colors = ["white","red", "yellow", "orange","#f8df81", "#f6aa90", "#f6b4bf", "#d5b6d5",
+    #        "#badfda", "#9bd0b7", "#F2BFD7", "#F6D7E8", "#DCD0EA", "#F2E8CE", "#F1DCC5"]
+    
+    #init list for frames for saving the gif
+    gif_frames = []
+
     # make canvas
     canvas = t.Screen()
     canvas.mode("world")
     canvas.setworldcoordinates(0,0,w,h)
-    canvas.bgcolor("black")
+    canvas.bgcolor("white")
 
     # making pencil
     pencil = t.Turtle()
@@ -107,7 +116,14 @@ def curtle_drawing(coordinates, h, w):
     pencil.penup()
     canvas.tracer(0)
 
-    for line in coordinates:
+    #begin drawing process
+    # adding a frame capture interval to speed up the process of drawing coords and saving
+    #capture every 5th move (this may be adjusted)
+
+    frameCapInterval = 10
+
+    #we're gonna edit the original drawing portion
+    for i, line in enumerate(coordinates):
         
         temp = line[0]
         temp[1] = h - temp[1]
@@ -115,18 +131,62 @@ def curtle_drawing(coordinates, h, w):
         pencil.color(random.choice(colors))
 
         # start draw
-        for spot in line:
+        for j, spot in enumerate(line):
             pencil.pendown()
             if spot == line[0]:
                 spot[1] = h - spot[1]
             
             spot[1] = h - spot[1]
             pencil.goto(spot)
+
+            #here we are going to capture the frame every nth(whichever val we decide on)
+            if j % frameCapInterval == 0:
+                # capture the current frame/drawing result for the GIF
+                # here we use ps since turtle doesnt allow for jpg/png saving natively
+                # saving the color as well
+                #canvas.getcanvas().postscript(file="processed/frame.ps", colormode='color')
+                # for optimization sake we are gonna comment out this line and switch it 
+                psData = canvas.getcanvas().postscript(colormode="color")
+                # converting the .ps file to RGBA
+                #frame = Image.open("processed/frame.ps").convert("RGBA")
+                # ^ for opti sake, swapping this line so we don't need to worry about PIL opening and closing files over and over
+                #using BytesIO to speed up program so that we dont need to work with the filesystem
+                #also makes more sense to avoid clutter, and since our data is temp
+                gifFrame = Image.open(io.BytesIO(psData.encode())).convert("RGBA")
+                #create a new black background image so we can save results with the black background
+                # to ensure that it is a fully opaque black background, rather than using "black" we're 
+                # gonna use (0,0,0,255)
+                # https://johndecember.com/html/spec/colorrgbadec.html
+                #background = Image.new("RGBA", (w,h), (0,0,0,255))
+                #using alpha composite 
+                #gifFrame = Image.alpha_composite(background, frame)
+                # append the frame 
+                gif_frames.append(gifFrame)
+
         #print ("endline")
         canvas.update()
         pencil.penup()
+    
+    #final canvas update
     canvas.tracer(1)
     canvas.update()
+
+    # saving the final drawing result as an image before we continue 
+    #similiar process to gif, only we will be saving the end version
+    #canvas.getcanvas().postscript(file="processed/results.ps", colormode='color')
+    #results = Image.open("processed/results.ps").convert("RGBA")
+    finalPSData = canvas.getcanvas().postscript(colormode="color")
+    results = Image.open(io.BytesIO(finalPSData.encode())).convert("RGBA")
+    #background = Image.new("RGBA", (w,h), (0,0,0,255))
+    #finalImg = Image.alpha_composite(background, results)
+    results.save(f"processed/{finalImgName}_drawing.png")
+
+    #saving the stop-motion animated gif
+    #we may need to adjust duration
+    gif_frames[0].save(f"processed/{finalImgName}.gif", save_all=True,
+                        append_images=gif_frames[1:], duration=10, loop=0)
+    
+    #input to continue to the next drawing process
     input("Press enter to continue: ")
 
 
@@ -154,12 +214,11 @@ if __name__ == '__main__':
                 coords, cH, cW = extract_contours(processedImg)
                 print(f"Image #{count}'s contours extracted")
 
+                #generating our imagefinalname field to pass to curtle_drawing
+                # this is just using our same count format
+                saveImgName = f"results_{count}"
                 #drawing function for image
-                curtle_drawing(coords, cH, cW)
-
-                #save the turtle img
-
-                #saving the gif of turtle drawing
+                curtle_drawing(coords, cH, cW, saveImgName)
 
                 #update count for sequential file formats(assuming we use this)
                 count += 1
